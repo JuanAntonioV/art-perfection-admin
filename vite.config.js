7ac57e8665171a0d45f88334df0c315b07ca52d1
Juan Antonio Vivaldy <juananthonio23@gmail.com>
2023-04-10 19:15:35 +0700

    project setup - add alias @ for all folder path

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+    plugins: [react()],
+    resolve: {
+        alias: {
+            '@': '/src',
+            '@components': '/src/components',
+            '@pages': '/src/pages',
+            '@utils': '/src/utils',
+            '@styles': '/src/styles',
+            '@assets': '/src/assets',
+            '@hooks': '/src/hooks',
+        },
+    },
+});
